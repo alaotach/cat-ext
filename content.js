600907977f47c6e55825bf440849f2292005fac0
alaotach <aryanmi2001@gmail.com>
2025-12-19 19:58:02 +0530

    cat walking at the bottom

--- content.js
+++ content.js
@@ -2,3 +2,24 @@ const cat = document.createElement("div");
 cat.id = "cat";
 cat.style.backgroundImage = `url('${chrome.runtime.getURL("image.png")}')`;
 document.body.appendChild(cat);
+
+let pos = 0;
+let dir = 1;
+const speed = 1; //pixels per frame
+
+function walk() {
+    setInterval(() => {
+        pos += dir * speed;
+        const screenW = window.innerWidth;
+        if (pos + 80 >= screenW) {
+        dir = -1;
+        cat.style.transform = "scaleX(-1)";
+        }
+        if ( pos <= 0) {
+            dir = 1;
+            cat.style.transform = "scaleX(1)";
+        }
+        cat.style.left = pos + "px";
+    }, 16);
+}
+walk();
